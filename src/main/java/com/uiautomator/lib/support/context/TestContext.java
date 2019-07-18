a97eeb008d45bc7facb2526b1e90a32fab6cd0d3
caihua <caihuadaye@outlook.com>
2019-07-18 16:27:14 +0800

    'update:增加返回字节数组的截图方法'

--- src/main/java/com/uiautomator/lib/support/context/TestContext.java
+++ src/main/java/com/uiautomator/lib/support/context/TestContext.java
@@ -1,20 +1,65 @@
 package com.uiautomator.lib.support.context;
 
+import android.app.Instrumentation;
+import android.app.UiAutomation;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.os.Build;
 import android.os.Environment;
 
 import androidx.annotation.NonNull;
 import androidx.test.core.app.ApplicationProvider;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.uiautomator.Configurator;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject2;
 
+import com.uiautomator.lib.support.log.UIAutomatorLogger;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
 public class TestContext {
+    private Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
+    /**
+     * 暴露UIDevice中的一些方法
+     */
+    static final int API_LEVEL_ACTUAL = Build.VERSION.SDK_INT
+            + ("REL".equals(Build.VERSION.CODENAME) ? 0 : 1);
+    static final int DEFAULT_UIAUTOMATION_FLAGS = 0;
+    UiAutomation getUiAutomation() {
+        int flags = Configurator.getInstance().getUiAutomationFlags();
+        if (API_LEVEL_ACTUAL > Build.VERSION_CODES.M) {
+            return instrumentation.getUiAutomation(flags);
+        } else {
+            // Custom flags not supported prior to N.
+            if (flags != DEFAULT_UIAUTOMATION_FLAGS) {
+                UIAutomatorLogger.e( "UiAutomation flags not supported prior to N - ignoring.");
+            }
+            return instrumentation.getUiAutomation();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
     private static final TestContext ourInstance = new TestContext();
     private ToastAccessibilityEventListener toastAccessibilityEventListener = new ToastAccessibilityEventListener();
     public static TestContext getInstance() {
@@ -24,6 +69,8 @@ public class TestContext {
     private TestContext() {
         getInstrumentation().getUiAutomation().setOnAccessibilityEventListener(toastAccessibilityEventListener);
     }
+
+
     public Map<String, String> getToastMessage(){
         String pkg =  toastAccessibilityEventListener.getToastMessage().packageName;
         String message =  toastAccessibilityEventListener.getToastMessage().message;
@@ -82,5 +129,119 @@ public class TestContext {
         ClipData clipData = ClipData.newPlainText("uiautomator_text", text);
         //添加ClipData对象到剪切板中
         clipboardManager.setPrimaryClip(clipData);
+    }
+
+
+    public static final String SHOT_PATH = "/UIAUTOMATOR_SCREEN_SHOT";
+    /**
+     * 截图失败返回false
+     * @return
+     */
+    public boolean screenShot(UiDevice device, String fileName){
+        String path = TestContext.getInstance().getExternalStorageDirectory()+ SHOT_PATH;
+        File pathFile = new File(path);
+        if (!pathFile.exists()) {
+            boolean success = pathFile.mkdirs();
+            if(!success){
+                UIAutomatorLogger.e("创建目录失败:"+path);
+                return false;
+            }
+        }
+        File shot = new File(String.format("%s/%s", path, fileName));
+        return device.takeScreenshot(shot);
+    }
+
+    /**
+     * png pic
+     * @param device
+     * @return
+     */
+    public byte[] screenShot(UiDevice device,  float scale, int quality){
+        Bitmap screenshot = getUiAutomation().takeScreenshot();
+        if (screenshot == null) {
+            return null;
+        }
+        BufferedOutputStream bos = null;
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            bos = new BufferedOutputStream(byteArrayOutputStream);
+            if (bos != null) {
+                screenshot.compress(Bitmap.CompressFormat.PNG, quality, bos);
+                bos.flush();
+            }
+            return byteArrayOutputStream.toByteArray();
+        } catch (IOException ioe) {
+            UIAutomatorLogger.e( "failed to save screen shot to file"+ioe.getMessage());
+            return null;
+        } finally {
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException ioe) {
+                    // Ignore
+                }
+            }
+            screenshot.recycle();
+        }
+    }
+    /**
+     * png pic
+     * @param device
+     * @return
+     */
+    public byte[] screenShot(UiDevice device){
+        return screenShot(device, 1.0f, 90);
+    }
+    /**
+     * png pic
+     * @param device
+     * @return
+     */
+    public byte[] screenShot(UiDevice device, UiObject2 uiObject,  float scale, int quality){
+        Bitmap screenshot = getUiAutomation().takeScreenshot();
+        if (screenshot == null) {
+            return null;
+        }
+        if (uiObject == null) {
+            return null;
+        }
+        Rect rect = uiObject.getVisibleBounds();
+        int wi = screenshot.getWidth();
+        Bitmap real = Bitmap.createBitmap(screenshot, rect.left, rect.top, rect.width(), rect.height());
+        BufferedOutputStream bos = null;
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            bos = new BufferedOutputStream(byteArrayOutputStream);
+            if (bos != null) {
+                real.compress(Bitmap.CompressFormat.PNG, quality, bos);
+                bos.flush();
+            }
+            return byteArrayOutputStream.toByteArray();
+        } catch (IOException ioe) {
+            UIAutomatorLogger.e( "failed to save screen shot to file"+ioe.getMessage());
+            return null;
+        } finally {
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException ioe) {
+                    // Ignore
+                }
+            }
+            real.recycle();
+            screenshot.recycle();
+        }
+    }
+    /**
+     * png pic
+     * @param device
+     * @return
+     */
+    public byte[] screenShot(UiDevice device, UiObject2 uiObject){
+       return screenShot(device, uiObject, 1.0f, 90);
+    }
+
+    public String uuid(){
+        return Build.FINGERPRINT;
     }
 }
